f30489e846ce806dc6be7a642326611534dd489f
PressXToWin <test@example.com>
2023-02-07 18:14:13 +0700

    add some logging, wip

--- homework.py
+++ homework.py
@@ -3,9 +3,17 @@ from dotenv import load_dotenv
 import telegram
 import time
 import requests
+import logging
+import sys
+from http import HTTPStatus
+
+from logging import StreamHandler
+
+logging.basicConfig(level=logging.DEBUG, format='%(asctime)s [%(levelname)s] %(message)s')
+logger = logging.getLogger(__name__)
+handler = StreamHandler(stream=sys.stdout)
 
 load_dotenv()
-
 
 PRACTICUM_TOKEN = getenv('PRACTICUM_TOKEN')
 TELEGRAM_TOKEN = getenv('TELEGRAM_TOKEN')
@@ -14,7 +22,6 @@ TELEGRAM_CHAT_ID = getenv('TELEGRAM_CHAT_ID')
 RETRY_PERIOD = 600
 ENDPOINT = 'https://practicum.yandex.ru/api/user_api/homework_statuses/'
 HEADERS = {'Authorization': f'OAuth {PRACTICUM_TOKEN}'}
-
 
 HOMEWORK_VERDICTS = {
     'approved': 'Работа проверена: ревьюеру всё понравилось. Ура!',
@@ -25,33 +32,50 @@ HOMEWORK_VERDICTS = {
 
 def check_tokens():
     if not PRACTICUM_TOKEN or not TELEGRAM_TOKEN or not TELEGRAM_CHAT_ID:
+        logging.critical('Необходимый токен не найден.')
         raise NameError('Необходимый токен не найден.')
 
 
 def send_message(bot, message):
-    bot.send_message(TELEGRAM_CHAT_ID, message)
+    try:
+        bot.send_message(TELEGRAM_CHAT_ID, message)
+    except Exception:
+        logger.error('Отправка сообщения не удалась')
+    else:
+        logger.debug('Сообщение отправлено успешно')
 
 
 def get_api_answer(timestamp):
     payload = {'from_date': timestamp}
     response = requests.get(url=ENDPOINT, headers=HEADERS, params=payload)
+    if response.status_code != HTTPStatus.OK:
+        logger.error('Ошибка получения ответа от эндпоинта')
+    else:
+        logger.debug('Ответ от API получен')
     return response.json()
 
 
 def check_response(response):
-    timestamp = response['current_date']
-    homeworks = response['homeworks']
-    if len(homeworks) == 0:
-        homework = None
-    else:
-        homework = homeworks[0]
-    return homework, timestamp
+    try:
+        timestamp = response['current_date']
+        homeworks = response['homeworks']
+        if len(homeworks) == 0:
+            homework = None
+        else:
+            homework = homeworks[0]
+        return homework, timestamp
+    except KeyError:
+        logger.error('Содержание ответа от API не соответствует ожидаемому')
 
 
 def parse_status(homework):
     homework_name = homework['homework_name']
     homework_status = homework['status']
-    verdict = HOMEWORK_VERDICTS[homework_status]
+    try:
+        verdict = HOMEWORK_VERDICTS[homework_status]
+    except Exception:
+        logger.error('Hеожиданный статус домашней работы')
+        verdict = homework_status
 
     return f'Изменился статус проверки работы "{homework_name}". {verdict}'
 
@@ -71,6 +95,8 @@ def main():
             if homework:
                 message = parse_status(homework)
                 send_message(bot, message)
+            else:
+                logger.debug('Статус без изменений.')
 
         except Exception as error:
             message = f'Сбой в работе программы: {error}'
