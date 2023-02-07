509249fa8e517dde2547cf18d4a9f6ab8708fc56
PressXToWin <PressXToWin@users.noreply.github.com>
2023-02-07 21:37:03 +0700

    correct logging and error raising

---
+++ exceptions.py
@@ -0,0 +1,5 @@
+class RequestError(Exception):
+    pass
+
+class WrongStatusCode(Exception):
+    pass
--- homework.py
+++ homework.py
@@ -6,6 +6,7 @@ import requests
 import logging
 import sys
 from http import HTTPStatus
+from exceptions import RequestError, WrongStatusCode
 
 from logging import StreamHandler
 
@@ -31,71 +32,73 @@ HOMEWORK_VERDICTS = {
 
 
 def check_tokens():
-    """TODO: сделать докстринг. Формально докстринг есть"""
+    """Проверка наличия всех необходимых токенов"""
     if not PRACTICUM_TOKEN or not TELEGRAM_TOKEN or not TELEGRAM_CHAT_ID:
         logging.critical('Необходимый токен не найден.')
         raise NameError('Необходимый токен не найден.')
 
 
 def send_message(bot, message):
-    """TODO: сделать докстринг. Формально докстринг есть"""
+    """Отправка сообщения в Telegram"""
     try:
         bot.send_message(TELEGRAM_CHAT_ID, message)
     except Exception:
-        logger.error('Отправка сообщения не удалась')
+        logger.error(f'Отправка сообщения не удалась: {message}')
     else:
-        logger.debug('Сообщение отправлено успешно')
+        logger.debug(f'Сообщение отправлено успешно: {message}')
 
 
 def get_api_answer(timestamp):
-    """TODO: сделать докстринг. Формально докстринг есть"""
+    """Запрашиваем информацию от API Практикума"""
     payload = {'from_date': timestamp}
     try:
         response = requests.get(url=ENDPOINT, headers=HEADERS, params=payload)
     except requests.RequestException:
-        raise Exception
+        logger.error('Ошибка запроса к API Практикума')
+        raise RequestError('Ошибка запроса к API Практикума')
     else:
-        if response.status_code != 200:
-            logger.error('Ошибка получения ответа от эндпоинта')
-            raise Exception
+        if response.status_code != HTTPStatus.OK:
+            logger.error(f'Ошибка {response.status_code} при получении ответа от API Практикума')
+            raise WrongStatusCode(f'Ошибка {response.status_code} при получении ответа от API Практикума')
         else:
             logger.debug('Ответ от API получен')
-        return response.json()
+            return response.json()
 
 
 def check_response(response):
-    """TODO: сделать докстринг. Формально докстринг есть"""
+    """Проверяем ответ от API Практикума"""
     try:
         timestamp = response['current_date']
         homeworks = response['homeworks']
         if not isinstance(homeworks, list):
-            raise TypeError
+            logger.error('В ответе API homeworks не является списком')
+            raise TypeError('В ответе API homeworks не является списком')
         if len(homeworks) == 0:
-            homework = None
+            last_homework = None
         else:
-            homework = homeworks[0]
-        return homework, timestamp
-    except KeyError:
-        logger.error('Содержание ответа от API не соответствует ожидаемому')
-        raise KeyError
+            last_homework = homeworks[0]
+        return last_homework, timestamp
+    except KeyError as error:
+        logger.error(f'Содержание ответа от API не соответствует ожидаемому, {error}')
+        raise KeyError(f'Содержание ответа от API не соответствует ожидаемому, {error}')
 
 
 def parse_status(homework):
-    """TODO: сделать докстринг. Формально докстринг есть"""
+    """Парсим статус домашней работы"""
     try:
         homework_name = homework['homework_name']
         homework_status = homework['status']
-    except KeyError:
-        logger.error('Ключ не найден')
+    except KeyError as error:
+        logger.error(f'Не найден необходимый ключ в ответе API, {error}')
+        raise KeyError(f'Не найден необходимый ключ в ответе API, {error}')
     else:
         try:
             verdict = HOMEWORK_VERDICTS[homework_status]
-        except Exception:
-            logger.error('Hеожиданный статус домашней работы')
-            verdict = homework_status
-            raise Exception
-
-    return f'Изменился статус проверки работы "{homework_name}". {verdict}'
+        except KeyError:
+            logger.error(f'Hеожиданный статус домашней работы, {homework_status}')
+            raise KeyError(f'Hеожиданный статус домашней работы, {homework_status}')
+        else:
+            return f'Изменился статус проверки работы "{homework_name}". {verdict}'
 
 
 def main():
@@ -105,6 +108,7 @@ def main():
 
     bot = telegram.Bot(token=TELEGRAM_TOKEN)
     timestamp = int(time.time())
+    last_error_message = ''
 
     while True:
         try:
@@ -115,10 +119,13 @@ def main():
                 send_message(bot, message)
             else:
                 logger.debug('Статус без изменений.')
+            last_error_message = ''
 
         except Exception as error:
             message = f'Сбой в работе программы: {error}'
-            send_message(bot, message)
+            if last_error_message != message:
+                send_message(bot, message)
+                last_error_message = message
 
         finally:
             time.sleep(RETRY_PERIOD)
